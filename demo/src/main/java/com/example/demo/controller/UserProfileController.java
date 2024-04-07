1306106a76a238982ee459e8a8c80b3c9c5e537c
kmj <orcaorka34@gmail.com>
2024-04-07 19:36:39 +0900

    ** demo 프로젝트 ver1
    - demo 라는 이름의 spring 프로젝트를 생성하여 Controller 생성 및 동작 확인

---
+++ demo/src/main/java/com/example/demo/controller/UserProfileController.java
@@ -0,0 +1,71 @@
+package com.example.demo.controller;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.mapper.UserProfileMapper;
+import com.example.demo.model.UserProfile;
+
+import jakarta.annotation.PostConstruct;
+
+@RestController
+public class UserProfileController 
+{
+	
+	private UserProfileMapper mapper;
+	
+	private Map<String, UserProfile> userMap;
+	
+	public UserProfileController(UserProfileMapper mapper) 
+	{
+		this.mapper = mapper;
+	}
+	
+	@PostConstruct
+	public void init() 
+	{
+		userMap = new HashMap<String, UserProfile>();
+		userMap.put("1", new UserProfile("1", "홍길동", "111-1111", "서울시 강남구 대치1동"));
+		userMap.put("2", new UserProfile("2", "홍길자", "111-1112", "서울시 강남구 대치2동"));
+		userMap.put("3", new UserProfile("3", "홍길순", "111-1113", "서울시 강남구 대치3동"));
+	}
+	
+	@GetMapping("/user/{id}")
+	public UserProfile getUserProfile(@PathVariable("id") String id) {
+		return userMap.get(id);
+	}
+	
+	@GetMapping("/user/all")
+	public List<UserProfile> getUserProfileList() {
+		return new ArrayList<UserProfile>(userMap.values());
+	}
+	
+	@PutMapping("/user/{id}")
+	public void putUserProfile(@PathVariable("id") String id, @RequestParam("name") String name, @RequestParam("phone") String phone, @RequestParam("address") String address) {
+		UserProfile userProfile = new UserProfile(id, name, phone, address);
+		userMap.put(id, userProfile);
+	}
+
+	@PostMapping("/user/{id}")
+	public void postUserProfile(@PathVariable("id") String id, @RequestParam("name") String name, @RequestParam("phone") String phone, @RequestParam("address") String address) {
+		UserProfile userProfile = userMap.get(id);
+		userProfile.setName(name);
+		userProfile.setPhone(phone);
+		userProfile.setAddress(address);
+	}
+	
+	@DeleteMapping("/user/{id}")
+	public void deleteUserProfile(@PathVariable("id") String id) {
+		userMap.remove(id);
+	}
+}
